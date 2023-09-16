04a4e6671ab9171e36dc189f17c5d481d035b972
metamesy <josiasgo4@gmail.com>
2023-09-16 13:23:48 -0300

    novas att

--- arquivos/armazenamento/armazenamento.py
+++ arquivos/armazenamento/armazenamento.py
@@ -1,15 +1,21 @@
+import re
 def converter(x):
     return float(f"{x / (2 ** 20) : .2f}")
 
 
 def percentual(y):
     espTotal = 0
-    for k in range(len(y)):
-        y[k] = converter(y[k])
-        espTotal += y[k]
-    for k in range(len(y)):
-        y[k] = float(f"{y[k] / espTotal * 100 : .2f}")
-    return y
+    percentuais = []
+
+    for valor in y:
+        espTotal += converter(valor)
+
+    for valor in y:
+        percentual_calculado = float(f"{converter(valor) / espTotal * 100 : .2f}")
+        percentuais.append(percentual_calculado)
+
+    return percentuais
+
 
 
 def media(z):
@@ -19,38 +25,32 @@ def media(z):
 
 dados = open("dados.txt", "r")
 lista = dados.readlines()
-inf = []
-dic = []
 total = []
-
-#for i in range(len(lista)):
-    #inf.append(lista[i].split(" "))
-
-#for i in range(len(lista)):
-    #total.append(int(inf[i][1]))
-
+for i in range(len(lista)):
+    lista[i] = (re.sub(" +", " ", lista[i]))
+    lista[i] = (lista[i].replace("\n", "")).split(" ")
+    total.append(int(lista[i][1]))
+dic = []
 porcentagem = percentual(total)
-
-for i, (usuario, armazenamento, perc) in enumerate(zip(inf, total, porcentagem), start = 1):
+for i, (inf, perc) in enumerate(zip(lista, porcentagem), start=1):
     dic.append({
-        "Nr": i,
-        "Usuario": usuario[0],
-        "Espaço utilizado": armazenamento,
-        "% do uso": perc
+       'Nr': i,
+        'Usuario': inf[0],
+        'Armazenamento': converter(int(inf[1])),
+        'Percentual': perc
     })
 
 relatorio = open("relatorio.txt", "w")
 
-relatorio.write("ACME Inc.               Uso do espaço em disco pelos usuários\n"
-                "------------------------------------------------------------------------\n"
-                "Nr.  Usuário        Espaço utilizado     % do uso\n")
+relatorio.write('''ACME Inc. -> Uso do espaço em disco pelos usuários\n
+------------------------------------------------------------------------\n
+Nr.    Usuário       Espaço utilizado     % do uso\n''')
 
 for item in dic:
-    relatorio.write(f"{item['Nr']: 2}   {item['Usuário']: < 15}    {item['Espaço utilizado']: < 18}")
+    relatorio.write(f"{item['Nr'] : 2}     {item['Usuario'] : <10}    {item['Armazenamento'] : <17}   {item['Percentual'] : < 6}%\n")
 
-
-relatorio.write(f"Espaço total ocupado: {converter(sum(total))} MB\n")
-relatorio.write(f"Espaço médio ocupado: {media(total)} MB")
+relatorio.write(f"\nEspaço total utilizado: {converter(sum(total))} MB")
+relatorio.write(f"\nMedia de uso: {converter(media(total)): .2f} MB")
 
 dados.close()
 relatorio.close()
